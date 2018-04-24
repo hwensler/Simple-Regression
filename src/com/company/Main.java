00d7ec3e53147e9e09b97cb7424586292ae299fb
hn <heathernolis@gmail.com>
2018-04-23 22:11:44 -0700

    added readme

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //TODO: hardcode file location here
+        String fileLocation = "C:\\Users\\Heather\\Documents\\Repositories\\Simple-Regression\\data\\auto insurance in sweden.xls";
+
+        //process file
+
     }
 }
--- src/com/company/SimpleRegression.java
+++ src/com/company/SimpleRegression.java
@@ -41,7 +41,7 @@ public class SimpleRegression {
 
         return input;
     }
-    
+
 
 
 }
